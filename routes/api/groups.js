9a7dc19d13d1a8c74e2caf1ed76d3a6076ee238e
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-02-04 15:34:19 -0600

    added routes to add member to a group and to delete a group

--- routes/api/groups.js
+++ routes/api/groups.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const mongojs = require("mongojs");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 
-// Load Game model
 const Groups = require("../../models/Groups");
 
+// Create a new group
 router.post("/", (req, res) => {
   console.log(req.body);
   Groups.create(req.body).then(group => {
@@ -14,6 +15,7 @@ router.post("/", (req, res) => {
   });
 });
 
+// Retrieve all groups
 router.get("/", (req, res) => {
   Groups.find({
   }).then(groups => {
@@ -21,10 +23,50 @@ router.get("/", (req, res) => {
   });
 });
 
+// Retrieve all groups with specified game type
 router.get("/type", (req, res) => {
   Groups.find({ gameType: req.body.gameType })
   .then(groups => {
     res.json(groups);
+  });
+});
+
+// Add a member to a group
+router.put("/add-member/:id", (req, res) => {
+  Groups.find({ _id: mongojs.ObjectId(req.params.id) }, (error, found) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const members = found[0].currentMembers;
+      members.push(req.body.name);
+      Groups.updateOne(
+        {
+          _id: mongojs.ObjectId(req.params.id)
+        },
+        {
+          $set: {
+            currentMembers: members
+          }
+        },
+        (error, edited) => {
+          if (error) {
+            console.log(error);
+          } else {
+            res.send(edited);
+          }
+        }
+      );
+    }
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  Groups.deleteOne({ _id: mongojs.ObjectId(req.params.id) }, (error, found) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(found);
+    }
   });
 });
 
