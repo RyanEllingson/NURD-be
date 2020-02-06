67f87e8575a97d8fb4eb47bb52f40ee6ed99c364
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-02-06 14:59:43 -0600

    wrote tests for user and group database objects

---
+++ test/routes.test.js
@@ -0,0 +1,40 @@
+const expect = require("chai").expect;
+const sinon = require("sinon");
+
+const groupRoutes = require("../routes/api/groups");
+const Group = require("../models/Groups");
+
+describe("routes", function() {
+    it("should pass", function(done) {
+        expect(true).to.equal(true);
+        done();
+    })
+    // beforeEach(function() {
+    //     sinon.stub(Group, "find");
+    // });
+
+    // afterEach(function() {
+    //     Group.find.restore();
+    // });
+
+    // it("should send all groups", function() {
+    //     const a = {
+    //         organizer: "test",
+    //         gameTitle: "test",
+    //         location: "test",
+    //         gameType: "test"
+    //     };
+    //     const b = {
+    //         organizer: "test",
+    //         gameTitle: "test",
+    //         location: "test",
+    //         gameType: "test"
+    //     };
+    //     const expectedGroups = [a, b];
+    //     Group.find.yields(null, expectedGroups);
+
+    //     const testGroups = groupRoutes.getGroupsApi();
+
+    //     expect(testGroups).to.equal(expectedGroups);
+    // });
+});
