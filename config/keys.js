fb1b93ed1247be0b859f8c7ea7c57af2aef613d5
RyanEllingson <ellin150@umn.edu>
2020-02-12 02:29:22 -0600

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI: process.env.MONGODB_URI || "mongodb://localhost/nurddb",
-  secretOrKey: process.env.SECRET_KEY || "secret",
+  secretOrKey: process.env.SECRET_KEY
   videoApiKey: process.env.VIDEO_APIKEY || "6e8c7cd85d2a38d0c9484d9a593046a7"
 };
 
